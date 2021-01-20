b0b44c91cd99c2602d12355b4af1c864c151ae21
Nick Ater <nickolas.ater@gmail.com>
2021-01-19 21:41:31 -0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let name: string;
+  import Map from './map.svelte';
+</script>
+
+<main>
+  <Map />
+</main>
+
+<style>
+</style>
---
+++ src/map.svelte
@@ -0,0 +1,30 @@
+<script>
+  const blocks = document.querySelectorAll('#block');
+  for (let index = 0; index < blocks.length; index++) {
+    const element = array[index];
+  }
+</script>
+
+<main>
+  <div id="table">
+    <div class="block" />
+  </div>
+</main>
+
+<style>
+  #table {
+    width: 800px;
+    height: 800px;
+    background-color: gray;
+    display: grid;
+    grid-template-columns: repeat(20, 40px);
+    grid-template-rows: repeat(20, 40px);
+    margin: auto;
+  }
+
+  .block {
+    width: 40px;
+    height: 40px;
+    background-color: black;
+  }
+</style>
